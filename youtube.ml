7996592ddfa974b034cfc9a36d5b88e1c3908528
ArnaudParant <ard.parant@gmail.com>
2014-10-24 15:04:08 -0500

    Youtube discography is now calling build_wiki_data instead of doing it itself

--- youtube.ml
+++ youtube.ml
@@ -1,11 +1,15 @@
 open Utils
 
+(******************************************************************************
+********************************** Utils **************************************
+*******************************************************************************)
+
 let is_youtube_uri uri =
   let str_uri = Ptype.string_of_uri uri in
   try ignore (Youtube_http.get_video_id_from_url str_uri); true
   with _ -> false
 
-let video_of_song wiki_title (_, title, _) =
+let video_uri_of_song wiki_title (_, title, _) =
   lwt videos = Youtube_http.search_video (wiki_title ^.^ title) 1 in
   let _, _, video_str_url, _, _ = List.hd videos in
   Lwt.return (Ptype.uri_of_string video_str_url)
@@ -20,47 +24,64 @@ let wrapper data_of_topic uri =
   lwt id, title, str_url, summary, categories = video_of_uri uri in
   let topics, r_topics = categories in
   let empty = ([], [], []) in
-  lwt uris, subjects, links =
-      Lwt_list.dep_fold_left data_of_topic empty [topics;r_topics]
-  in
+  lwt data = Lwt_list.dep_fold_left data_of_topic empty [topics;r_topics] in
+  let uris, subjects, links = data in
   lwt tag_ids = Tag.Of_Content.makes subjects in
   lwt () = Tag.Of_Content.assign tag_ids uri in
   lwt _ = Link.insert links in
   Lwt.return uris
 
+let append_data previous_data new_data =
+  let p_uris, p_subjects, p_links = previous_data in
+  let n_uris, n_subjects, n_links = new_data in
+  n_uris@p_uris, n_subjects@p_subjects, n_links@p_links
+
+(******************************************************************************
+********************************** Wiki ***************************************
+*******************************************************************************)
+
 lwt talk_about = Tag.Of_Link.make "Talk about"
 lwt mentioned_by = Tag.Of_Link.make "Mentioned by"
 
+let build_wiki_data uris (t1, t2) p_data wiki_data =
+  let _,wiki_title,_,_,_,wiki_urls = wiki_data in
+  let wuris = List.map Ptype.uri_of_string wiki_urls in
+  let links = Link.build_inter_link t1 t2 uris wuris in
+  append_data p_data (wuris, [wiki_title], links)
+
 let wiki uri =
-  let data_of_topic (p_uris, p_subjects, p_links) topic =
-    lwt _,wiki_title,_,_,_,wiki_urls = Freebase_http.get_topics topic in
-    let wuris = List.map Ptype.uri_of_string wiki_urls in
-    let links = Link.build_inter_link [talk_about] [mentioned_by] [uri] wuris in
-    Lwt.return (wuris@p_uris,
-                wiki_title::p_subjects,
-                links@p_links)
+  let data_of_topic p_data topic =
+    lwt wiki_data = Freebase_http.get_topics topic in
+    let tags = [talk_about], [mentioned_by] in
+    Lwt.return (build_wiki_data [uri] tags p_data wiki_data)
   in
   wrapper data_of_topic uri
 
+(******************************************************************************
+******************************* Discography ***********************************
+*******************************************************************************)
 
 lwt made_by = Tag.Of_Link.make "Made by"
 lwt made = Tag.Of_Link.make "Made"
 lwt same_band = Tag.Of_Link.make "Same Band"
 
 let discography uri =
-  let data_of_topic (p_uris, p_subjects, p_links) topic =
-    lwt _,wiki_title,_,_,_,wiki_str_urls = Freebase_http.get_topics topic in
-    let wiki_uris = List.map Ptype.uri_of_string wiki_str_urls in
+  let data_of_topic p_data topic =
+    lwt wiki_data = Freebase_http.get_topics topic in
+    let _,wiki_title,_,_,_,wiki_str_urls = wiki_data in
     lwt song = Dbpedia_http.get_discography wiki_title in
-    lwt video_uris = Lwt_list.map_p (video_of_song wiki_title) song in
+    lwt video_uris = Lwt_list.map_p (video_uri_of_song wiki_title) song in
     let v_uris = uri::video_uris in
-    let wiki_links = Link.build_inter_link [made_by] [made] v_uris wiki_uris in
     let video_links = Link.build_each_on_all [same_band] v_uris in
-    Lwt.return (video_uris@wiki_uris@p_uris,
-                wiki_title::p_subjects,
-                video_links@wiki_links@p_links)
+    let tags = [made_by], [made] in
+    let new_data = build_wiki_data v_uris tags p_data wiki_data in
+    Lwt.return (append_data new_data (video_uris, [], video_links))
   in
   wrapper data_of_topic uri
+
+(******************************************************************************
+********************************** Switch *************************************
+*******************************************************************************)
 
 let switch uri =
   print_endline "Youtube";
