f773d2d63dcd1281be066e57616de2e2e5fe0d38
ArnaudParant <ard.parant@gmail.com>
2014-11-13 22:04:45 +0100

    The bot is now able to add tags on wiki pages

--- dbpedia.ml
+++ dbpedia.ml
@@ -1,7 +1,18 @@
 open Utils
 
-let is_wikipedia_uri uri = false
+let is_wikipedia_uri uri =
+  Dbpedia_http.is_wikipedia_uri (Ptype.string_of_uri uri)
 
 let get uri =
   print_endline "Dbpedia";
+  let open Dbpedia_record.Basic in
+  let str_uri = Ptype.string_of_uri uri in
+  lwt data = Lwt_list.hd (Dbpedia_http.get_basic_informations_by_uri str_uri) in
+  let format_subject subject =
+    let subject = Str.replace_first (Str.regexp "^.*:") "" subject in
+    Str.global_replace (Str.regexp "[_\.]") " " subject
+  in
+  let subjects = List.map format_subject data.subject in
+  lwt tag_ids = Tag.Of_Content.makes subjects in
+  lwt () = Tag.Of_Content.assign tag_ids uri in
   Lwt.return []
