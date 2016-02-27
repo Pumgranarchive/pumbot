4f2f470b1b39d5f65a5a69dea96c9676a342df90
ArnaudParant <ard.parant@gmail.com>
2016-02-27 11:00:10 +0100

    [bot] Added start header to the log

--- bot/main.ml
+++ bot/main.ml
@@ -6,7 +6,15 @@
 let init options =
   Pumgrana_http.set_pumgrana_api_uri options.ArgParser.api_host
 
+let header () =
+  print_endline "";
+  print_endline "#############################################################";
+  print_endline "####################### Start Crawling ######################";
+  print_endline "#############################################################";
+  print_endline ""
+
 let main () =
+  let () = header () in
   let input_list = List.tl (Array.to_list Sys.argv) in
   let uri_list, options = ArgParser.get_options input_list in
   let () = init options in
